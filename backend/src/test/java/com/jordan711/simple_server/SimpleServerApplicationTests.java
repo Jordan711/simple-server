15374db80af44178ea5920551315b2cf683799f4
jordan711 <jordan.feng@hotmail.com>
2026-05-30 15:37:20 -0400

    Starter code from https://start.spring.io/

---
+++ backend/src/test/java/com/jordan711/simple_server/SimpleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jordan711.simple_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
